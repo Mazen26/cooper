0e28e98d3fddd68028ff955ce29c0d9dd2fdb54b
Mazen <mmeezzoo26@me.com>
2016-03-30 19:36:50 +0200

    fix for person.js back out of ionic branch

--- src/person.js
+++ src/person.js
@@ -15,15 +15,15 @@ Person.prototype.performance = function (distance) {
 Person.prototype.resultForMale = function(distance) {
     var age = this.age;
     switch (true) {
-        case (13 <= age <= 14):
+        case (13 <= age && age <= 14):
             switch (true) {
                 case (distance < 2100):
                     return 'Poor';
-                case (2100 <= distance < 2200):
+                case (2100 <= distance && distance < 2200):
                     return 'Below average';
-                case (2200 <= distance <  2400):
+                case (2200 <= distance && distance <  2400):
                     return 'Average';
-                case (2400 <= distance < 2700):
+                case (2400 <= distance && distance < 2700):
                     return 'Above average';
                 case (2700 <= distance):
                     return 'Excellent';
@@ -31,15 +31,15 @@ Person.prototype.resultForMale = function(distance) {
             break;
 
 
-        case (15 <= age <= 16):
+        case (15 <= age && age <= 16):
             switch (true) {
                 case (distance < 2200):
                     return 'Poor';
-                case (2200 <= distance < 2300):
+                case (2200 <= distance && distance < 2300):
                     return 'Below average';
-                case (2300 <= distance < 2500):
+                case (2300 <= distance && distance < 2500):
                     return 'Average';
-                case (2500 <= distance < 2800):
+                case (2500 <= distance && distance < 2800):
                     return 'Above average';
                 case (2800 <= distance):
                     return 'Excellent';
@@ -47,60 +47,60 @@ Person.prototype.resultForMale = function(distance) {
             break;
 
 
-        case (17 <= age <=19):
+        case (17 <= age && age <=19):
             switch (true) {
                 case (distance < 2300):
                     return 'Poor';
-                case (2300 <= distance < 2500):
+                case (2300 <= distance && distance < 2500):
                     return 'Below average';
-                case (2500 <= distance < 2700):
+                case (2500 <= distance && distance < 2700):
                     return 'Average';
-                case (2700 <= distance < 3000):
+                case (2700 <= distance && distance < 3000):
                     return 'Above average';
                 case (3000 <= distance):
                     return 'Excellent';
             }
             break;
 
-        case (20 <= age <=29):
+        case (20 <= age && age <=29):
             switch (true) {
                 case (distance < 1600):
                     return 'Poor';
-                case (1600<= distance < 2200):
+                case (1600 <=  distance && distance < 2200):
                     return 'Below average';
-                case (2200 <= distance < 2400):
+                case (2200 <= distance && distance < 2400):
                     return 'Average';
-                case (2400<= distance < 2800):
+                case (2400 <= distance && distance < 2800):
                     return 'Above average';
-                case (2800<= distance):
+                case (2800 <= distance):
                     return 'Excellent';
             }
             break;
 
-        case (30 <= age <=39):
+        case (30 <= age && age <=39):
             switch (true) {
                 case (distance < 1500):
                     return 'Poor';
-                case (1500 <= distance < 2000):
+                case (1500 <= distance && distance < 2000):
                     return 'Below average';
-                case (2000 <= distance < 2300):
+                case (2000 <= distance && distance < 2300):
                     return 'Average';
-                case (2300 <= distance < 2700):
+                case (2300 <= distance && distance < 2700):
                     return 'Above average';
                 case (2700<= distance):
                     return 'Excellent';
             }
             break;
 
-        case (40 <= age <= 49):
+        case (40 <= age && age <= 49):
             switch (true) {
                 case (distance < 1400):
                     return 'Poor';
-                case (1400 <= distance < 1700):
+                case (1400 <= distance && distance < 1700):
                     return 'Below average';
-                case (1700 <= distance < 2100):
+                case (1700 <= distance && distance < 2100):
                     return 'Average';
-                case (2100 <= distance < 2500):
+                case (2100 <= distance && distance < 2500):
                     return 'Above average';
                 case (2500<= distance):
                     return 'Excellent';
@@ -111,11 +111,11 @@ Person.prototype.resultForMale = function(distance) {
             switch (true) {
                 case (distance < 1300):
                     return 'Poor';
-                case (1300 <= distance < 1600):
+                case (1300 <= distance && distance < 1600):
                     return 'Below average';
-                case (1600 <= distance < 2000):
+                case (1600 <= distance && distance < 2000):
                     return 'Average';
-                case (2000 <= distance < 2400):
+                case (2000 <= distance && distance < 2400):
                     return 'Above average';
                 case (2400 <= distance):
                     return 'Excellent';
@@ -128,15 +128,15 @@ Person.prototype.resultForMale = function(distance) {
 Person.prototype.resultForFemale = function(distance) {
     var age = this.age;
     switch (true) {
-        case (13 <= age <= 14):
+        case (13 <= age && age <= 14):
             switch (true) {
                 case (distance < 1500):
                     return 'Poor';
-                case (1500 <= distance < 1600):
+                case (1500 <= distance && distance < 1600):
                     return 'Below average';
-                case (1600 <= distance < 1900):
+                case (1600 <= distance && distance < 1900):
                     return 'Average';
-                case (1900 <= distance < 2000):
+                case (1900 <= distance && distance < 2000):
                     return 'Above average';
                 case (2000 <= distance):
                     return 'Excellent';
@@ -144,15 +144,15 @@ Person.prototype.resultForFemale = function(distance) {
             break;
 
 
-        case (15<= age <= 16):
+        case (15<= age && age <= 16):
             switch (true) {
                 case (distance < 1600):
                     return 'Poor';
-                case (1600 <= distance < 1700):
+                case (1600 <= distance && distance < 1700):
                     return 'Below average';
-                case (1700 <= distance < 2000):
+                case (1700 <= distance && distance < 2000):
                     return 'Average';
-                case (2000 <= distance < 2100):
+                case (2000 <= distance && distance < 2100):
                     return 'Above average';
                 case (2100 <= distance):
                     return 'Excellent';
@@ -160,30 +160,30 @@ Person.prototype.resultForFemale = function(distance) {
             break;
 
 
-        case (17 <= age <= 19):
+        case (17 <= age && age <= 19):
             switch (true) {
                 case (distance < 1700):
                     return 'Poor';
-                case (1700 <= distance < 1800):
+                case (1700 <= distance && distance < 1800):
                     return 'Below average';
-                case (1800 <= distance < 2100):
+                case (1800 <= distance && distance < 2100):
                     return 'Average';
-                case (2100 <= distance < 2300):
+                case (2100 <= distance && distance < 2300):
                     return 'Above average';
                 case (2300 <= distance ):
                     return 'Excellent';
             }
             break;
 
-        case (20 <= age <= 29):
+        case (20 <= age && age <= 29):
             switch (true) {
                 case (distance < 1500):
                     return 'Poor';
-                case (1500 <= distance < 1800):
+                case (1500 <= distance && distance < 1800):
                     return 'Below average';
-                case (1800 <= distance < 2200):
+                case (1800 <= distance && distance < 2200):
                     return 'Average';
-                case (2200 <= distance < 2700):
+                case (2200 <= distance && distance < 2700):
                     return 'Above average';
                 case (2700 <= distance):
                     return 'Excellent';
@@ -191,30 +191,30 @@ Person.prototype.resultForFemale = function(distance) {
             }
             break;
 
-        case (30 <= age <= 39):
+        case (30 <= age && age <= 39):
             switch (true) {
                 case (distance < 1400):
                     return 'Poor';
-                case (1400 <= distance < 1700):
+                case (1400 <= distance && distance < 1700):
                     return 'Below average';
-                case (1700 <= distance < 2000):
+                case (1700 <= distance && distance < 2000):
                     return 'Average';
-                case (2000 <= distance < 2500):
+                case (2000 <= distance && distance < 2500):
                     return 'Above average';
                 case (2500 <= distance):
                     return 'Excellent';
             }
             break;
 
-        case (40 <= age <= 49):
+        case (40 <= age && age <= 49):
             switch (true) {
                 case (distance < 1200):
                     return 'Poor';
-                case (1200 <= distance < 1500):
+                case (1200 <= distance && distance < 1500):
                     return 'Below average';
-                case (1500 <= distance < 1900):
+                case (1500 <= distance && distance < 1900):
                     return 'Average';
-                case (1900 <= distance < 2300):
+                case (1900 <= distance && distance < 2300):
                     return 'Above average';
                 case (2300 <= distance):
                     return 'Excellent';
@@ -225,11 +225,11 @@ Person.prototype.resultForFemale = function(distance) {
             switch (true) {
                 case (distance < 1100):
                     return 'Poor';
-                case (1100<= distance < 1400):
+                case (1100<= distance && distance < 1400):
                     return 'Below average';
-                case (1400 <= distance < 1700):
+                case (1400 <= distance && distance < 1700):
                     return 'Average';
-                case (1700 <= distance < 2200):
+                case (1700 <= distance && distance < 2200):
                     return 'Above average';
                 case (2200<= distance):
                     return 'Excellent';
