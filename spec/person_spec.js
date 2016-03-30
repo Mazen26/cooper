775b38a0e56d70705e01d91b4682106ea94739f8
Mazen <mmeezzoo26@me.com>
2016-03-30 12:32:15 +0200

    person spec done / cleaned jasmin initiation files

---
+++ spec/person_spec.js
@@ -0,0 +1,18 @@
+describe("Person", function() {
+    var person;
+
+    beforeEach(function() {
+        person = new Person({gender: 'male', age: 36 });
+    });
+
+    it("should have gender as male", function() {
+        expect(person.gender).toBeDefined();
+        expect(person.gender).toEqual("male");
+    });
+
+    it("should have age of 36", function() {
+        expect(person.gender).toBeDefined();
+        expect(person.age).toEqual(36);
+    });
+    
+});
